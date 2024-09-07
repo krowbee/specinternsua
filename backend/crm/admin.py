458ca4a2c8d68ceb7919644869a037a23431040a
specinterns <specinternsua@gmail.com>
2024-09-07 03:44:27 +0300

    Completed CRM DB and USER MODELS

--- backend/crm/admin.py
+++ backend/crm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Project, Task, Assignment, Event
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Assignment)
+admin.site.register(Event)
--- backend/crm/models.py
+++ backend/crm/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+# Модель проєкту
+class Project(models.Model):
+    title = models.CharField("Назва проєкту", max_length=256)
+    description = models.TextField("Опис")
+    members = models.ManyToManyField("authorization.CustomUser", verbose_name="Команда проєкту", related_name='team_members')
+    start_date = models.DateField("Дата старту")
+    duration_in_weeks = models.IntegerField("Тривалість", null=True)
+    project_link = models.URLField("Посилання на проєкт", null=True, max_length=256)
+    preview = models.URLField("Посилання на зображення для прев'ю", null=True, max_length=256)
+    status = models.CharField(max_length=13, null=True, choices=(
+                                        ("ACTIVE", "Active"),
+                                        ("NOT STARTED", "Not started"),
+                                        ("FINISHED", "Finished")))
+    task_asignee = models.ForeignKey("authorization.CustomUser", verbose_name=("TeamLead"),
+                                     on_delete=models.CASCADE, related_name="team_lead")
+
+
+# Модель сертифікату
+class Certificate(models.Model):
+    number = models.CharField(unique=True, null=True, max_length=128)
+    user = models.ForeignKey("authorization.CustomUser", verbose_name="Власник сертифікату", on_delete=models.CASCADE,
+                             related_name="certificate_owner")
+
+
+# Модель задачі
+class Task(models.Model):
+    title = models.CharField("Тема завдання", max_length=126)
+    description = models.TextField("Завдання")
+    assigned_to = models.ForeignKey("authorization.CustomUser", verbose_name="Отримувач", on_delete=models.CASCADE,
+                                    related_name='assigned_tasks')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project_tasks')
+    due_date = models.DateField("Термін виконання", null=True)
+    status = models.CharField("Статус", max_length=12, choices=(
+                                                ("IN PROGRESS", "In progress"),
+                                                ("COMPLETED", "Completed")))
+    priority = models.CharField("Приорітет задачі", max_length=6, choices=(
+                                                            ("LOW", "Low"),
+                                                            ("MEDIUM", "Medium"),
+                                                            ("HIGH", "High")))
+
+
+# Модель подій в CRM
+class Event(models.Model):
+    event_type = models.CharField("Тип події", null=True, max_length=16, choices=(
+                                                        ("MEETING", "Зустріч"),
+                                                        ("CODE REVIEW", "Код рев'ю"),
+                                                        ("PROGRESS UPDATE", "Оновлення прогресу")))
+    description = models.TextField("Опис події")
+    date_time = models.DateTimeField("Дата та час події", null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+# Модель призначення задач
+class Assignment(models.Model):
+    task = models.ForeignKey(Task, verbose_name="Задача", on_delete=models.CASCADE)
+    executor = models.ForeignKey("authorization.CustomUser", verbose_name="Виконавець", on_delete=models.CASCADE)
+    assignment_date = models.DateTimeField("Дата призначення", auto_now_add=True)
+    status = models.CharField("Статус", max_length=12, choices=(
+                                                ("IN PROGRESS", "In progress"),
+                                                ("COMPLETED", "Completed")))
